a75f23fffb1fcea2d2ccd5bfef0522ca89a2c769
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-09-22 22:07:04 +0200

    Add login hints

--- src/ui/LoginPage.tsx
+++ src/ui/LoginPage.tsx
@@ -5,10 +5,14 @@ export const LoginPage = () => {
   const login = useLogin();
   return (
     <div className="flex flex-col h-full bg-base-200">
-      <div className="card w-96 bg-base-100 shadow-xl m-auto">
+      <div className="card w-2/5 bg-base-100 shadow-xl m-auto">
         <div className="card-body">
           <h2 className="card-title">React Admin</h2>
           <p>Please enter your username and password.</p>
+          <p>
+            (try <code className="text-secondary">janedoe/password</code> or{" "}
+            <code className="text-secondary">johndoe/password</code>)
+          </p>
           <Form onSubmit={(values) => login(values)}>
             <TextInput label="Username" source="username" />
             <TextInput label="Password" source="password" type="password" />
