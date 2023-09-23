a6e0b3782bca9800abe9b50e85923fca7118e7e7
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-09-23 16:22:18 +0200

    Show theme live on focusing

--- src/ui/ThemeSelector.tsx
+++ src/ui/ThemeSelector.tsx
@@ -7,6 +7,7 @@ import {
   Item,
   ListBox,
   Popover,
+  ItemProps,
 } from "react-aria-components";
 
 export const useTheme = (): [string, (theme: string) => void] => {
@@ -43,6 +44,11 @@ export const ThemeSelector = () => {
       menuTrigger="focus"
       className="form-control w-full"
       selectedKey={theme}
+      onOpenChange={(isOpen) => {
+        if (!isOpen) {
+          document.documentElement.setAttribute("data-theme", theme);
+        }
+      }}
       onSelectionChange={(selectedKey) => {
         if (!selectedKey) return;
         const theme = selectedKey.toString();
@@ -66,50 +72,57 @@ export const ThemeSelector = () => {
                 })
               }
               key={themeName}
-              textValue={themeName}
               id={themeName}
+              textValue={themeName}
             >
-              <div className="border-base-content/20 hover:border-base-content/40 overflow-hidden rounded-lg border outline outline-2 outline-offset-2 outline-transparent">
-                <div
-                  data-theme={themeName}
-                  className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
-                >
-                  <div className="grid grid-cols-5 grid-rows-3">
-                    <div className="bg-base-200 col-start-1 row-span-2 row-start-1"></div>{" "}
-                    <div className="bg-base-300 col-start-1 row-start-3"></div>{" "}
-                    <div className="bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex flex-col gap-1 p-2">
-                      <div className="font-bold">{themeName}</div>{" "}
-                      <div className="flex flex-wrap gap-1">
-                        <div className="bg-primary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-                          <div className="text-primary-content text-sm font-bold">
-                            A
-                          </div>
-                        </div>{" "}
-                        <div className="bg-secondary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-                          <div className="text-secondary-content text-sm font-bold">
-                            A
-                          </div>
-                        </div>{" "}
-                        <div className="bg-accent flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-                          <div className="text-accent-content text-sm font-bold">
-                            A
-                          </div>
-                        </div>{" "}
-                        <div className="bg-neutral flex aspect-square w-5 items-center justify-center rounded lg:w-6">
-                          <div className="text-neutral-content text-sm font-bold">
-                            A
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              {(state) => (
+                <ThemeItem isFocused={state.isFocused} theme={themeName} />
+              )}
             </Item>
           ))}
         </ListBox>
       </Popover>
     </ComboBox>
+  );
+};
+
+const ThemeItem = (props: { isFocused: boolean; theme: string }) => {
+  useEffect(() => {
+    if (props.isFocused) {
+      document.documentElement.setAttribute("data-theme", props.theme);
+    }
+  }, [props.isFocused, props.theme]);
+  return (
+    <div className="border-base-content/20 hover:border-base-content/40 overflow-hidden rounded-lg border outline outline-2 outline-offset-2 outline-transparent">
+      <div
+        data-theme={props.theme}
+        className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+      >
+        <div className="grid grid-cols-5 grid-rows-3">
+          <div className="bg-base-200 col-start-1 row-span-2 row-start-1"></div>{" "}
+          <div className="bg-base-300 col-start-1 row-start-3"></div>{" "}
+          <div className="bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex flex-col gap-1 p-2">
+            <div className="font-bold">{props.theme}</div>{" "}
+            <div className="flex flex-wrap gap-1">
+              <div className="bg-primary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
+                <div className="text-primary-content text-sm font-bold">A</div>
+              </div>{" "}
+              <div className="bg-secondary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
+                <div className="text-secondary-content text-sm font-bold">
+                  A
+                </div>
+              </div>{" "}
+              <div className="bg-accent flex aspect-square w-5 items-center justify-center rounded lg:w-6">
+                <div className="text-accent-content text-sm font-bold">A</div>
+              </div>{" "}
+              <div className="bg-neutral flex aspect-square w-5 items-center justify-center rounded lg:w-6">
+                <div className="text-neutral-content text-sm font-bold">A</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
