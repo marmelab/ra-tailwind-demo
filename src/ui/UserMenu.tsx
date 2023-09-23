f1b6741bbb853bab96d9d0b1d72778d97c1b4067
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-09-23 10:02:48 +0200

    Remove unnecessary user menu links

--- src/ui/UserMenu.tsx
+++ src/ui/UserMenu.tsx
@@ -16,15 +16,6 @@ export const UserMenu = () => {
         className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
       >
         <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li>
-          <a>Settings</a>
-        </li>
-        <li>
           <button onClick={logout}>Logout</button>
         </li>
       </ul>
