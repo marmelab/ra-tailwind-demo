8a7b8942db6f4c0baf049df5604d7e7f93cefbe9
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-10-18 17:03:07 +0200

    Refactor datagrid

--- src/ui/Datagrid.tsx
+++ src/ui/Datagrid.tsx
@@ -3,19 +3,27 @@ import { RaRecord, useListContext } from "ra-core";
 import {
   ColumnDef,
   SortingState,
+  Table,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { Button } from "react-aria-components";
-import { BarsArrowDownIcon, BarsArrowUpIcon } from "@heroicons/react/24/outline";
+import {
+  BarsArrowDownIcon,
+  BarsArrowUpIcon,
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/24/outline";
 
 export const Datagrid = <RecordType extends RaRecord = RaRecord>({
   columns,
 }: {
   columns: ColumnDef<RecordType, any>[];
 }) => {
-  const { data, page, perPage, setPage, setPerPage, setSort, sort, total } =
+  const { data, page, perPage, setSort, sort, total } =
     useListContext<RecordType>();
 
   const pagination = useMemo(
@@ -55,7 +63,10 @@ export const Datagrid = <RecordType extends RaRecord = RaRecord>({
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <th key={header.id} className={header.column.columnDef.meta?.headerClassName}>
+                <th
+                  key={header.id}
+                  className={header.column.columnDef.meta?.headerClassName}
+                >
                   {header.isPlaceholder ? null : header.column.getCanSort() ? (
                     <button
                       className="flex items-center gap-2 cursor-pointer select-none"
@@ -69,7 +80,7 @@ export const Datagrid = <RecordType extends RaRecord = RaRecord>({
                             ? "ASC"
                             : "DESC";
 
-						setSort({
+                        setSort({
                           field: header.column.id,
                           order,
                         });
@@ -107,72 +118,81 @@ export const Datagrid = <RecordType extends RaRecord = RaRecord>({
           ))}
         </tbody>
       </table>
-      <div className="flex items-center gap-2">
-        <div className="join">
-          <Button
-            className="join-item btn btn-sm"
-            onPress={() => setPage(1)}
-            isDisabled={!table.getCanPreviousPage()}
-          >
-            {"<<"}
-          </Button>
-          <Button
-            className="join-item btn btn-sm"
-            onPress={() => setPage(page - 1)}
-            isDisabled={!table.getCanPreviousPage()}
-          >
-            {"<"}
-          </Button>
-          <Button
-            className="join-item btn btn-sm"
-            onPress={() => setPage(page + 1)}
-            isDisabled={!table.getCanNextPage()}
-          >
-            {">"}
-          </Button>
-          <Button
-            className="join-item btn btn-sm"
-            onPress={() => table.setPageIndex(table.getPageCount() - 1)}
-            isDisabled={!table.getCanNextPage()}
-          >
-            {">>"}
-          </Button>
-        </div>
-        <span className="flex items-center gap-1">
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span className="flex items-center gap-1">
-          | Go to page:
-          <input
-            className="input input-sm input-bordered w-24"
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              setPage(page);
-            }}
-          />
-        </span>
-        <select
-          className="select select-sm"
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            setPerPage(Number(e.target.value));
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-        <span>Total: {total}</span>
-      </div>
+      <Pagination table={table} />
     </>
   );
 };
 
+const Pagination = ({ table }: { table: Table<any> }) => {
+  const { page, setPage, setPerPage, total } = useListContext();
+  return (
+    <div className="flex items-center gap-2">
+      <div className="join">
+        <Button
+          className="join-item btn btn-sm"
+          onPress={() => setPage(1)}
+          isDisabled={!table.getCanPreviousPage()}
+        >
+          <span className="sr-only">First page</span>
+          <ChevronDoubleLeftIcon className="w-6 h-6" />
+        </Button>
+        <Button
+          className="join-item btn btn-sm"
+          onPress={() => setPage(page - 1)}
+          isDisabled={!table.getCanPreviousPage()}
+        >
+          <span className="sr-only">Previous page</span>
+          <ChevronLeftIcon className="w-6 h-6" />
+        </Button>
+        <Button
+          className="join-item btn btn-sm"
+          onPress={() => setPage(page + 1)}
+          isDisabled={!table.getCanNextPage()}
+        >
+          <span className="sr-only">Next page</span>
+          <ChevronRightIcon className="w-6 h-6" />
+        </Button>
+        <Button
+          className="join-item btn btn-sm"
+          onPress={() => setPage(table.getPageCount())}
+          isDisabled={!table.getCanNextPage()}
+        >
+          <span className="sr-only">Last page</span>
+          <ChevronDoubleRightIcon className="w-6 h-6" />
+        </Button>
+      </div>
+      <span className="flex items-center gap-1">
+        <div>Page</div>
+        <strong>
+          {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        </strong>
+      </span>
+      <span className="flex items-center gap-1">
+        | Go to page:
+        <input
+          className="input input-sm input-bordered w-24"
+          type="number"
+          defaultValue={table.getState().pagination.pageIndex + 1}
+          onChange={(e) => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0;
+            setPage(page);
+          }}
+        />
+      </span>
+      <select
+        className="select select-sm"
+        value={table.getState().pagination.pageSize}
+        onChange={(e) => {
+          setPerPage(Number(e.target.value));
+        }}
+      >
+        {[10, 20, 30, 40, 50].map((pageSize) => (
+          <option key={pageSize} value={pageSize}>
+            Show {pageSize}
+          </option>
+        ))}
+      </select>
+      <span>Total: {total}</span>
+    </div>
+  );
+};
